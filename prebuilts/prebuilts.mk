d5a3957e549d0580911e38bdfd1168274357d1a8
pkm774 <mprabhat774@gmail.com>
2024-05-31 15:59:02 +0530

    bcr: Append PRODUCT_SOONG_NAMESPACES

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -1,3 +1,6 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/bcr
+
 # Copy permission files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bcr/prebuilts/product/etc/permissions,$(TARGET_COPY_OUT_PRODUCT)/etc/permissions)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bcr/prebuilts/product/etc/sysconfig,$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig)
